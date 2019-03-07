228dfb47b35c8bec5f44998b40ed67f16caa0e53
jmlekston <conso.lekston@gmail.com>
2019-03-07 18:29:58 +0100

    alpha release

--- blackjack.py
+++ blackjack.py
@@ -14,6 +14,9 @@ class Card(object):
 
   def __str__(self):
     return '{0} of {1}'.format(self.figure, self.color)
+  
+  def answer(self, req):
+    return req._('{0} of {1}').format(req._(self.figure), req._(self.color))
 
 class Shoe(object):
   def __init__(self):
@@ -87,6 +90,14 @@ class Hand(object):
       msg += delimiter + str(card)
       delimiter = ", "
     return msg
+  
+  def answer(self, req):
+    msg = ''
+    delimiter = ''
+    for card in self.cards:
+      msg += delimiter + card.answer(req)
+      delimiter = ", "
+    return msg
 
 class Game(object):
   # game state
@@ -112,13 +123,14 @@ class Game(object):
     self.standard_odds = 1
     self.state = self.START 
     self.player_action = None
-    self.player_money = 100
+    self.player_bankroll = 100
     self.player_bet = 0
     self.player_double = False
     self.player_hit_counter = 0
     self.player_insurance = None
     self.number_of_packets = 1
     self.player_hand = Hand()
+    self.dealer_hidden_card = None
     self.dealer_hand = Hand()
     self.shoe = Shoe()
     self._logger = logging.getLogger(self.__class__.__name__.lower())
@@ -149,6 +161,7 @@ class Game(object):
     self.player_action_counter = 0
     self.player_insurance = None
     self.player_bet = None
+    self.dealer_hidden_card = None
     if req.intent.slot('bet').first().value != None:
       try:
         self.player_bet = int(req.intent.slot('bet').first().value)    
@@ -157,7 +170,7 @@ class Game(object):
     
     if not self.player_bet:
       return req.agent.ask('bet', req._('What is your bet?'))    
-    self.player_money -=  self.player_bet
+    self.player_bankroll -=  self.player_bet
     self.state = self.BEGIN_OF_TURN
     return self
 
@@ -170,7 +183,7 @@ class Game(object):
       self.player_hand.add(self.shoe.draw())
       self.player_hand.add(self.shoe.draw())
       self.dealer_hand.add(self.shoe.draw())
-      self.dealer_hand.add(self.shoe.draw())
+      self.dealer_hidden_card = self.shoe.draw()
     except:
       self.state = self.END_OF_TURN
       req.agent.answer(req._('No more card'))
@@ -220,6 +233,8 @@ class Game(object):
   
   def dealer_actions(self, req):
     self._logger.debug('dealer_actions')
+    self.dealer_hand.add(self.dealer_hidden_card)
+    self.dealer_hidden_card = None
     req.agent.answer(req._('Dealer hidden card is a {0}').format(req._(self.dealer_hand.last())))      
     while True:
       if self.dealer_hand.evaluate() > self.player_hand.evaluate() or self.dealer_hand.evaluate() > 17:
@@ -237,19 +252,21 @@ class Game(object):
 
   def end_of_turn(self, req):
     self._logger.debug('end_of_turn')
+    req.agent.answer(req._('Your hand contains : {0}').format(game.player_hand.answer(req)))
+    req.agent.answer(req._('The dealer hand contains (only visible cards) : {0}').format(game.dealer_hand.answer(req)))
     if self.player_hand.evaluate() > 21:
       req.agent.answer(req._('Unfortunately! your hand is over 21. You lost'))
     elif self.dealer_hand.evaluate() > 21:
       req.agent.answer(req._('Congratulation! dealer hand is over 21. You won'))
-      self.player_money += self.player_bet * 2        
+      self.player_bankroll += self.player_bet * 2        
     elif self.player_hand.evaluate() == self.dealer_hand.evaluate():
       req.agent.answer(req._('Tie, no one won'))
     elif self.player_hand.evaluate() == 21:
       req.agent.answer(req._('Blackjack! You won'))
-      self.player_money += math.ceil(self.player_bet * 3/2)        
+      self.player_bankroll += math.ceil(self.player_bet * 3/2)        
     elif self.player_hand.evaluate() > self.dealer_hand.evaluate():
       req.agent.answer(req._('Congratulation! You won.'))
-      self.player_money += self.player_bet * 2        
+      self.player_bankroll += self.player_bet * 2        
     elif self.player_hand.evaluate() < self.dealer_hand.evaluate():
       req.agent.answer(req._('Unfortunately! You lost'))
     else:
@@ -291,7 +308,25 @@ class Game(object):
 # This entity will be shared among training data since it's not language specific
 
 en_help="""
-Let's play blackjack
+Blackjack is a multiplayer card game where players play against one dealer. 
+Players who want entering the game  bet. 
+At the begin the dealer gives 2 cards face up to each player in game and take two cards one face down.
+At any time if a player hand is exactly 21, he is doing a blackjack. He can reveive no more cards.
+At any time if a player hand is over 21, he is busting and loose automatically. 
+During their turns, players can
+"double"  for  doubling their bet and receiving one last card. 
+"hit" for receiving one more card
+"stand" to stop receving card.
+when all players finished 
+The delear reveal his facedown card and draw until his hand reach 17.
+If the player hand is over 21, he lost the turn and his bet.
+If the player and dealer hand is of equal value, there is a tie and the player take his bet. 
+If the player hand  21, he is doing "blackjack" and receive 3/2 times his bet as bonus.
+If the player hand is over the dealer hand, he wins and receive 1 times his bet as bonus.
+If the dealer hand is over 21, he is busting and the player wins, receiving 1 times the bet as bonus.
+Other case the wins and the player loose his bet
+Hand value is equal to the cards value sum. "Face card"value is 10. Ace card is a special card wich value is 11 when hand value can be less or equal to 21 or 1 in other case 
+At any time, you can "See your hand", "See the dealer hand", "See your banckroll" or "Quit"
 """
 
 
@@ -324,43 +359,59 @@ def en_data(): return """
 
 %[blackjack/bet]
   I bet @[bet]
-  Again
+  again
   On more time
   deal for @[bet]
-
 
 @[bet](type=number)
   1
   2
   3
 
+%[blackjack/show_player_hand]
+  what is my hand
+  seed my hand
+  my hand
+ 
+%[blackjack/show_dealer_hand]
+  what is the dealer hand
+  see dealer hand
+  dealer hand
+  
+%[blackjack/bankroll]
+  how many chips I have
+  what is my bankroll
+  bankroll
+
 %[blackjack/quit]
   I quit
 
-% [blackjack/help]
-  Give me some advice
+%[blackjack/help]
+  give me some advice
+  hints
 """
 
 fr_help="""
-Le blackjack est un jeu de carte de un à plusieurs joueurs qui jouent contre un croupier. 
+Le blackjack est un jeu de carte de un à plusieurs joueurs qui jouent contre un donneur. 
 Les  joueurs qui souhaitent jouer déposent un pari (par exemple "Je mise  2"). 
-Le croupier ensuite distribue 2 cartes face ouverte à chaque joueur et prend  deux cartes dont une face cachée.
+Le donneur ensuite distribue 2 cartes face ouverte à chaque joueur et prend  deux cartes dont une face cachée.
 Si à un moment le joueurs totalise 21, il fait "blackjack" il ne peut recevoir de carte supplémentaire
 Si à un moment le joueur dépasse 21, il saute et à automatiquement perdu
 les joueurs ont la possibilités chacun leur tour de 
 Soit "doubler" dans ce cas il une seule et dernière carte supplémentaire et double sa mise en cas de victoire 
 Soit "tirer" dans ce cas il reçoit une carte supplémentaire
 Soit "refuser" dans ce cas sa main est fait et il ne reçoit plus de carte supplémentaire. 
-Ensuite le croupier revèle sa carte caché et tire jusqu'à obtenir au moins 17.
+Ensuite le donneur revèle sa carte caché et tire jusqu'à obtenir au moins 17.
 Si la main du joeur totalise plus de 21, il a perdu et perd sa mise
-Si la main du  joueur et du croupier totalisent le même résultat il y a "égalité" tie et le joueur récupère sa mise
+Si la main du  joueur et du donneur totalisent le même résultat il y a "égalité" tie et le joueur récupère sa mise
 Si la main du joueur totalise 21, il fait "blackjack", il récupère une prime équivalente à  3/2 fois sa  mise
-Si la main du joueur totalise plus de point que celle du croupier le joueur gagne et récupère une prime de 1 fois sa mise
-Si la main du croupier depasse 21, il saute et le joueur gagne, il récupère une prime de 1 fois sa mise
-Dans le cas contraire, il croupier gagne et le joeur perd sa mise 
+Si la main du joueur totalise plus de point que celle du donneur le joueur gagne et récupère une prime de 1 fois sa mise
+Si la main du donneur depasse 21, il saute et le joueur gagne, il récupère une prime de 1 fois sa mise
+Dans le cas contraire, il donneur gagne et le joeur perd sa mise 
 Une main vaut la somme des valeurs des cartes qui la compose.
 Les cartes valent leur valeur chiffrée sauf pour les "bûches" (valet, dame , roi) qui valent 10.
 L'as est un peu spéciale, il vaut soit 1, soit 10 selon que la main depasse ou non 21. Ainsi As + 9 vaut 20, As + 9 + 2 vaut 12.
+A tout moment, vous pouvez "Voir votre main", "Voir la main du donneur, "Voir votre mise de fond" ou "Quitter"
 """
 
 @training('fr')
@@ -407,12 +458,31 @@ def fr_data(): return """
   2
   3
 
+%[blackjack/show_player_hand]
+  quelle est ma main
+  voir ma main
+  ma main
+
+%[blackjack/show_dealer_hand]
+  quelle est la main du donneur
+  voir la main du donneur
+  main du donneur
+
+%[blackjack/bankroll]
+  combien de jeton me reste-t-il
+  quelle est ma mise de fond
+  voir ma mise de fond
+  bankroll
+
 %[blackjack/quit]
   quitter
+  abandonner
   je quitte le jeu
 
-% [blackjack/help]
+%[blackjack/help]
   quelles sont les rêgles
+  aide
+  astuce
 """
 
 @translations('fr')
@@ -426,12 +496,12 @@ def fr_translations(): return {
   'What is your bet?' : 'Qu\'elle est votre mise?',
   'No more card' : 'Plus de carte',
   'You got a {0} and a {1}': 'Vous avez reçu {0} et {1}',
-  'Dealer got a {0} and a face down card':'Le croupier vient de recevoir {0} et une carte face cachée',
+  'Dealer got a {0} and a face down card':'Le donneur vient de recevoir {0} et une carte face cachée',
   'Your got a {0}': 'Vous avez reçu {0}',
-  'Dealer hidden card is a {0}': 'La carte face cachée du croupier est {0}',
-  'Dealer got a {0}':'Le croupier vient de recevoir {0}',
+  'Dealer hidden card is a {0}': 'La carte face cachée du donneur est {0}',
+  'Dealer got a {0}':'Le donneur vient de recevoir {0}',
   'Unfortunately! your hand is over 21. You lost':'Malheureusement! Votre main dépasse 21. Vous avez perdu',
-  'Congratulation! dealer hand is over 21. You won':'Félicitation! Le main du croupier dépasse 21. Vous avez gagné',
+  'Congratulation! dealer hand is over 21. You won':'Félicitation! Le main du donneur dépasse 21. Vous avez gagné',
   'Tie, no one won':'Egalité, personne n\'est vainqueur.',
   'Blackjack! You won':'Blackjack! Vous avez gagné',
   'Congratulation! You won.':'Félicitation! Vous avez gagner',
@@ -439,7 +509,28 @@ def fr_translations(): return {
   'Tie' : 'Egalité',
   'Shoe is empty': 'Le sabot est vide',
   'Create a new one' : 'Remplissage d\'un nouveau sabot',
-  'Bet for a new turn' : 'Misez pour jouer'
+  'Bet for a new turn' : 'Misez pour jouer',
+  'Your bankroll is {0} chips' : 'Votre mise de fond est {0} jeton(s)',
+  'Your hand contains : {0}' : 'Votre main contient : {0}',
+  'The dealer hand contains (only visible cards) : {0}' : 'La main du donneur contient (cartes visibles uniquement) : {0}',
+  '{0} of {1}': '{0} de {1}',
+  'ace':  'as',
+  '2':'2',
+  '3':'3',
+  '4':'4',
+  '5':'5',
+  '6':'6',
+  '7':'7',
+  '8':'8',
+  '9':'9',
+  '10':'10',
+  'jack':'valet',
+  'queen':'reine',
+  'king':'roi',
+  'clubs':  'trèfles',
+  'diamonds':'carreaux',
+  'hearts': 'coeurs',
+  'spades':'piques'
 }
 
 
@@ -495,11 +586,28 @@ def on_insurance(req):
   return game.apply_rule(req)
 """
 
+@intent('blackjack/show_player_hand')
+def on_show_player_hand(req):
+  global game
+  req.agent.answer(req._('Your hand contains : {0}').format(game.player_hand.answer(req)))
+  return req.agent.done()
+
+@intent('blackjack/show_dealer_hand')
+def on_show_dealer_hand(req):
+  global game
+  req.agent.answer(req._('The dealer hand contains (only visible cards) : {0}').format(game.dealer_hand.answer(req)))
+  return req.agent.done()
+
+@intent('blackjack/bankroll')
+def on_bankroll(req):
+  global game
+  req.agent.answer(req._('Your bankroll is {0} chips').format(game.player_bankroll))
+  return req.agent.done()
+
 @intent('blackjack/help')
 def on_help(req):
-  global game
-  game.player_action = game.HELP
-  return game.apply_rule(req)
+  req.agent.answer(req._(en_help))
+  return req.agent.done()
 
 @intent('blackjack/quit')
 def on_quit(req):
